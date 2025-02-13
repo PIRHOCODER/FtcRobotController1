cbc54178d574aa2c12822cedbf7aae43b7f61b0f
TechMaster15 <AnimeFan0205@gmail.com>
2025-02-13 15:26:37 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/LfrontSlide.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.Mechanisms;
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class LfrontSlide {
+    private Servo LfrontSlide;
+
+    public LfrontSlide(HardwareMap hardwareMap) {
+        LfrontSlide = hardwareMap.get(Servo.class, "LfrontSlide");
+    }
+
+//out Low Slide
+    public class outL implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            LfrontSlide.setPosition(0.3);
+            return false;
+        }
+    }
+
+    public Action outL() {
+        return new outL();
+    }
+
+//in Low Slide
+    public class inL implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            LfrontSlide.setPosition(0);
+            return false;
+        }
+    }
+
+    public Action inL() {
+        return new inL();
+    }
+}
